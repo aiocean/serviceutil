03e5ab19fb7c7661cc20fa4fbb3c567a8a9abd2f
openhoangnc <hoangnc.jp@gmail.com>
2022-09-23 16:11:12 +0700

    ignore grpc health check logs

--- logger/logger.go
+++ logger/logger.go
@@ -3,11 +3,44 @@ package logger
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+type ignoreHealthCheckCore struct {
+	c zapcore.Core
+}
+
+func (ig *ignoreHealthCheckCore) Enabled(lv zapcore.Level) bool {
+	return ig.c.Enabled(lv)
+}
+
+func (ig *ignoreHealthCheckCore) With(fs []zapcore.Field) zapcore.Core {
+	return ig.c.With(fs)
+}
+
+func (ig *ignoreHealthCheckCore) Check(e zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	return ig.c.Check(e, ce)
+}
+
+func (ig *ignoreHealthCheckCore) Write(e zapcore.Entry, fs []zapcore.Field) error {
+	if e.Level == zap.InfoLevel && strings.HasSuffix(e.Message, "code OK") {
+		for _, f := range fs {
+			if f.Key == "grpc.service" && strings.HasPrefix(f.String, "grpc.health") {
+				return nil
+			}
+		}
+	}
+
+	return ig.c.Write(e, fs)
+}
+
+func (ig *ignoreHealthCheckCore) Sync() error {
+	return ig.c.Sync()
+}
 
 func NewLogger(ctx context.Context) (*zap.Logger, error) {
 
@@ -36,7 +69,9 @@ func NewLogger(ctx context.Context) (*zap.Logger, error) {
 		ErrorOutputPaths:  []string{"stderr"},
 	}
 
-	logger, err := zapConfig.Build()
+	logger, err := zapConfig.Build(zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return &ignoreHealthCheckCore{c}
+	}))
 	if err != nil {
 		return nil, err
 	}
